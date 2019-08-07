7bcf0715499ecc9cbdc5e826034b0c601e01723e
adamgarza <agarza@thecreativefew.com>
2019-08-06 19:07:38 -0500

    Add new Contentful entry

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { graphql } from "gatsby"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 class BlogPost extends Component {
   render() {
-    const { title, content } = this.props.data.contentfulBlog
+    const { title, content, featureImage } = this.props.data.contentfulBlog
     return (
       <Layout>
         <SEO title={title} />
-        <h1>{title}</h1>
+        <Container className="post-container blog-container">
+          <Row>
+            <Col xs={12} sm={12} md={1} lg={1}></Col>
+            <Col xs={12} sm={12} md={5} lg={5} className="blog-post-container">
+              <h1>{title}</h1>
+              <img src={featureImage.fixed.src} />
+            </Col>
+            <Col
+              xs={12}
+              sm={12}
+              md={5}
+              lg={5}
+              dangerouslySetInnerHTML={{
+                __html: content.childMarkdownRemark.html,
+              }}
+            />
+
+            <Col xs={12} sm={12} md={1} lg={1}></Col>
+          </Row>
+        </Container>
+        {/* <h1>{title}</h1>
         <div
           dangerouslySetInnerHTML={{ __html: content.childMarkdownRemark.html }}
-        />
+        /> */}
       </Layout>
     )
   }
@@ -32,6 +56,12 @@ export const pageQuery = graphql`
       content {
         childMarkdownRemark {
           html
+          excerpt
+        }
+      }
+      featureImage {
+        fixed {
+          src
         }
       }
     }
